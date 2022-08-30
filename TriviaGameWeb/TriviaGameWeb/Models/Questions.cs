1aa53679c4561dc4458683d86bcc243b925b2f4f
juanvasquez <jgvasquez@my.milligan.edu>
2022-08-30 00:45:12 -0400

    displayed questions from the TriviaQuestions database in the Index page

---
+++ TriviaGameWeb/TriviaGameWeb/Models/Questions.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+using AppDomain;
+
+namespace TriviaGameWeb.Models
+{
+    public class Questions
+    {
+        private AppDomain.Models.TriviaQuestionsContext _context = new AppDomain.Models.TriviaQuestionsContext();
+        private AppDomain.Models.Triviaquestion question = new AppDomain.Models.Triviaquestion();
+        //public List<String> questions { get; set; } 
+
+        /*public Questions()
+        {
+        }*/
+
+        public List<AppDomain.Models.Triviaquestion> getQuestions()
+        {
+            var quest =  _context.Triviaquestions.ToList();
+
+            /*for(var idx = 0; idx < quest.Count; idx++)
+            {
+                questions.Insert(idx, quest[idx]);
+            }*/
+            return quest;    
+        }
+    }
+}
+
